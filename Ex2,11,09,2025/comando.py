b5a4a1c70b9959c01ae4760d518d04a446e39cdd
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-09-12 15:44:22 -0300

    Update comando.py

--- Ex2,11,09,2025/comando.py
+++ Ex2,11,09,2025/comando.py
@@ -7,19 +7,8 @@ find ~/ -name "ClassAmbiente.py"
 """
 
 """
-Resultado do comando:
-douglas@CodeGenius:~/Visual Studio/AulaInteligenciaArtificial/Ex2,11,09,2025$ python3 Main.py
-
-Posição 0: sujo
-Posição 1: limpo
-Posição 2: sujo
-Posição 3: limpo
-Posição 4: sujo
-Posição 5: limpo
-Posição 6: sujo
-Posição 7: limpo
-Posição 8: sujo
-Posição 9: limpo
+Rodar o Main:
+python3 Main.py
 
 """
 
